2b652fbeae6b56aa176d7236256805798d5bb736
Peter Schaarschmidt <pschaars@gmail.com>
2017-08-07 15:31:09 -0700

    Add files via upload

--- plots3.R
+++ plots3.R
@@ -1,13 +1,9 @@
 file <- "household_power_consumption.txt"                            
-power <- read.csv2(file, na.strings = "?")
+power <- read.csv(file, sep=";", na.strings = "?")
 power$datetime <- as.POSIXct(paste(power$Date, power$Time, sep = " "), format="%d/%m/%Y %H:%M:%S")
 power$Date <- as.Date(power$Date, "%d/%m/%Y")
 power <- power[power$Date >= "2007-02-01" & power$Date <= "2007-02-02",]
 power <- power[!is.na(power$Global_active_power), ]
-power$Global_active_power <- as.numeric(power$Global_active_power)
-power$Sub_metering_1 <- as.numeric(power$Sub_metering_1)
-power$Sub_metering_2 <- as.numeric(power$Sub_metering_2)
-power$Sub_metering_3 <- as.numeric(power$Sub_metering_3)
 png("plot3.png", width=480, height=480)
 plot(Sub_metering_1 ~ datetime, power, type="l", xlab="", ylab="Energy sub metering")
 lines(Sub_metering_2 ~ datetime, power, col="red")
